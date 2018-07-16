7432734013d78d8672e71da9ced144d4a1fe9fd7
Arjun Venkataswamy <avenkat2@gmail.com>
2018-07-16 09:35:30 -0500

    updates to factory_bot

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module OmnicalcParams
   class Application < Rails::Application
     config.generators do |g|
       g.test_framework nil
-      g.factory_girl false
+      g.factory_bot false
       g.scaffold_stylesheet false
     end
 
