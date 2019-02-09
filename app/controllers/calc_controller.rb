8bf3ea5b5bf11234760952d888f97ad4d39996f5
Will Fischer <wfische0@chicagobooth.edu>
2019-02-09 21:13:16 +0000

    with initial routing options in place

---
+++ app/controllers/calc_controller.rb
@@ -0,0 +1,26 @@
+class CalcController < ApplicationController
+  def flex_square
+    #"the_num" => "42"
+    
+    @num = params.fetch("the_num")
+    render("calc_templates/flexible_square.html.erb")
+  end
+  def flex_square_root
+    @num = params.fetch("the_num").to_f
+    @root = @num ** (0.5)
+    render("calc_templates/flexible_square_root.html.erb")
+  end
+  def flex_payment
+    @apr = params.fetch("apr").to_f / 120000
+    @duration = params.fetch("duration").to_i
+    @principal = params.fetch("principal").to_i
+    @payment = (@apr*@principal)/(1-((1+@apr)**(-12*@duration)))
+    render("calc_templates/flexible_payment.html.erb")
+  end
+  def flex_random
+    @small = params.fetch("small")
+    @large = params.fetch("large")
+    @num =  rand(@small.to_i..@large.to_i) #@small.to_f + rand(@large.to_f)
+    render("calc_templates/flexible_random.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get("/flexible/square/:the_num", {:controller => "calc" ,:action => "flex_square"})
+  get("/flexible/square_root/:the_num", {:controller => "calc" ,:action => "flex_square_root"})
+  get("/flexible/payment/:apr/:duration/:principal", {:controller => "calc" ,:action => "flex_payment"})
+  get("/flexible/random/:small/:large", {:controller => "calc" ,:action => "flex_random"})
+  
 end
